23b7798c3eeec3f38c1b015d15f3165c36d7fb9f
Matt Geruolo <mmg6297@psu.edu>
2024-08-10 20:55:46 -0400

    intitializing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+function MyButton({onClick,name}){
+    // Event handler function
+    return (
+      <button onClick={() => onClick(name)}>
+        Click Me
+      </button>
+    );
+  }
 
 function App() {
+  const handleClick = (name) => {
+    console.log('Button clicked with name:', name);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className = "App-header">
+        <h1> About Me</h1>
+      </div>
+      <div className ="app-container">
+        <h2> Hello world</h2>
+       
+      </div>
+      <MyButton onClick={handleClick} name ="hello world"/>
     </div>
   );
 }
